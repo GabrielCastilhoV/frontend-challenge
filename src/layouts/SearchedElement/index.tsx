ff42b0468c60693dbb72cd71cf4e2a86b9fa1e81
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-10-10 16:28:18 -0400

    chore: add button load more

--- src/layouts/SearchedElement/index.tsx
+++ src/layouts/SearchedElement/index.tsx
@@ -1,31 +1,75 @@
 /* eslint-disable @next/next/no-img-element */
-import { useBooks } from '@/contexts/BooksContexts';
+import { BooksData, useBooks } from '@/contexts/BooksContexts';
 import { Container, Content } from './styles';
 
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+import { RiLoader3Fill } from 'react-icons/ri';
+import { toast } from 'react-toastify';
 
 export const SearchedElement = () => {
-  const { books } = useBooks();
+  const { books, debouncedInput } = useBooks();
+  const [end, setEnd] = useState(3);
+  const [booksSliced, setBooksSliced] = useState<BooksData[]>([]);
+  const [prevInput, setPrevInput] = useState('');
+
+  useEffect(() => {
+    try {
+      setPrevInput(debouncedInput);
+      setBooksSliced(books.slice(0, end));
+
+      if (debouncedInput !== prevInput) {
+        setEnd(3);
+      }
+    } catch (error) {
+      toast.error('Book display errors');
+    }
+  }, [end, books, debouncedInput, prevInput]);
+
+  const handleLoadMore = () => {
+    setEnd(booksSliced.length + 3);
+  };
+
   return (
     <Container>
       <Content>
-        {books.map(book => (
-          <div className="booksInfo" key={book.id}>
-            <Link href={`book/${book.id}`}>
-              <a>
-                <img
-                  src={book.volumeInfo.imageLinks?.thumbnail}
-                  alt={book.volumeInfo?.title}
-                />
-                <div>
-                  <p>{book.volumeInfo.title}</p>
-                  <span>{book.volumeInfo.authors}</span>
-                </div>
-              </a>
-            </Link>
-          </div>
-        ))}
+        {books === undefined ? (
+          <>
+            <div>
+              <p>🙁 No results found..</p>
+            </div>
+          </>
+        ) : (
+          <>
+            {booksSliced.map(book => (
+              <div className="booksInfo" key={book.id}>
+                <Link href={`book/${book.id}`}>
+                  <a>
+                    <img
+                      src={book.volumeInfo.imageLinks?.thumbnail}
+                      alt={book.volumeInfo?.title}
+                    />
+                    <div>
+                      <p>{book.volumeInfo.title}</p>
+                      <span>{book.volumeInfo.authors}</span>
+                    </div>
+                  </a>
+                </Link>
+              </div>
+            ))}
+          </>
+        )}
       </Content>
+
+      {books !== undefined && end < books.length && (
+        <div className="loadMore" onClick={handleLoadMore}>
+          <button>
+            <RiLoader3Fill />
+            Load More
+          </button>
+        </div>
+      )}
     </Container>
   );
 };
--- src/layouts/SearchedElement/styles.ts
+++ src/layouts/SearchedElement/styles.ts
@@ -2,6 +2,40 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
   width: 100%;
+  margin-bottom: 5rem;
+
+  .loadMore {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 1rem;
+      padding: 1rem;
+
+      background: ${({ theme }) => theme.pink300};
+      color: #fff;
+      border-radius: 0.5rem;
+
+      font-size: 1rem;
+      font-weight: 700;
+
+      transition: filter 0.3s;
+
+      &:hover {
+        filter: brightness(1.1);
+      }
+
+      svg {
+        width: 26px;
+        height: 26px;
+      }
+    }
+  }
 `;
 
 export const Content = styled.div`
@@ -10,10 +44,16 @@ export const Content = styled.div`
   margin: 0 auto;
   padding: 2.65rem 1.2rem;
 
+  position: relative;
+
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 15px;
   justify-items: center;
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 
   .booksInfo {
     width: 100px;
@@ -30,16 +70,16 @@ export const Content = styled.div`
     }
 
     p {
-      font-family: 'SF Pro Text';
       font-weight: 700;
-      font-size: 0.8571rem;
+      font-size: 12px;
+
       margin-top: 9px;
     }
 
     span {
-      font-family: 'Roboto';
+      color: rgba(49, 49, 49, 0.8);
       font-weight: 900;
-      font-size: 0.714285rem;
+      font-size: 10px;
       margin-top: 5px;
     }
   }
