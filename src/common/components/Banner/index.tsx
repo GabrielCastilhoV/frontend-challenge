bea34eace2fad794a9d69e4b43efaf8d0c2b8235
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-10-10 16:41:22 -0400

    feat: add swiper slider

--- src/common/components/Banner/index.tsx
+++ src/common/components/Banner/index.tsx
@@ -2,14 +2,32 @@ import { Container } from './styles';
 import { CardBanner } from './CardBanner';
 import { HeaderBook } from '../HeaderBook';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
+
+SwiperCore.use([Navigation, Pagination, Autoplay]);
+
 export const Banner = () => {
   return (
     <Container>
       <HeaderBook title="Discover new book" nameLink="More" url="/" />
 
-      <div className="cards">
-        <CardBanner />
-      </div>
+      <Swiper
+        className="swiper"
+        spaceBetween={-50}
+        slidesPerView={1}
+        navigation={false}
+        autoplay={{ delay: 8000 }}
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide className="swiper-slider">
+          <CardBanner />
+        </SwiperSlide>
+
+        <SwiperSlide className="swiper-slider">
+          <CardBanner />
+        </SwiperSlide>
+      </Swiper>
     </Container>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,11 @@ import theme from '../styles/theme';
 import { ToastContainer } from 'react-toastify';
 import { Flip } from 'react-toastify';
 
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
